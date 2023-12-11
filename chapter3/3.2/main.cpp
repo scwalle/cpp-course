e3d93ea60456edd1e747635e16d43e4774530736
scwalle <scwaller@bluevalleyk12.net>
2023-12-11 00:49:31 -0600

    project 3.2 + chapter 3 prompts

--- chapter3/3.2/main.cpp
+++ chapter3/3.2/main.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    int num1, num2;
-    cout << "Enter 2 integers:\n";
-    cout << "1st integer:\n";
-    cin >> num1;
-    cout << "2nd integer:\n";
-    cin >> num2;
 
-    cout << "Sum:";
-    cout << num1 + num2;
-    cout << "\n";
+void twoReal(float a, float d, float rest){
+	float disc = ( sqrt(d) / (2*a) );
+	cout << "There are two real solutions:\n" 
+		 << "x = " << rest + disc << endl
+		 << "x = " << rest - disc;
+}
 
-    cout << "Product: ";
-    cout << num1 * num2;
-    cout << "\n";
+void twoComplex(float a, float d, float rest){
+	float imag = sqrt(-d) / (2*a);
+	cout << "There are two complex solutions:\n" 
+		 << "x = " << rest << " + " << imag << "i\n"
+		 << "x = " << rest << " - " << imag << "i\n";
+}
 
-    return 0;
+void oneReal(float rest){
+	cout << "There is one real solution:\n" 
+		 << "x = " << rest << endl;
+}
+
+void run(){
+	float a, b, c;
+	cout << "a: "; cin >> a;
+	cout << "b: "; cin >> b;
+	cout << "c: "; cin >> c;
+	
+	// discriminant
+	float d = b*b - (4 * a * c);
+	float rest = (-b / (2*a));
+	if (d > 0) { // 2 real roots
+		twoReal(a, d, rest);
+	} else if (d < 0) { // 2 complex roots
+		twoComplex(a, d, rest);
+	} else { // d == 0, 1 real root
+		oneReal(rest);
+		// return rest;
+	}
+}
+
+int main(){
+	/* cout.setf(ios::fixed); */
+	/* cout.setf(ios::showpoint); */
+	cout.precision(5);
+
+	do {
+		cout << endl;
+		run();
+
+		cout << endl << endl;
+		cout << "Run again? (y/n): ";
+		char loop;
+		cin >> loop;
+		if (loop == 'n' || loop == 'N') break;
+	} while (true);
+
+	return 0;
 }
