b14bb03340d4f522ec82d39a772d197aaef91e70
scwalle <scwaller@bluevalleyk12.net>
2023-12-12 13:05:51 -0600

    project 3.11

--- chapter3/3.11/main.cpp
+++ chapter3/3.11/main.cpp
@@ -7,28 +7,59 @@ bool doesntWork(char inp[], int len) {
 			case '1':
 			case '4':
 			case '7':
-				return false;
+				return true;
 		}
 	}
-	return true;
+	return false;
 }
 
-bool fixTime(char inp[], int len) {
-	for(int i = 0; i < len; i++){
-		switch (inp[i]) {
-			case '1':
-			case '4':
-			case '7':
-				return false;
-		}
-	}
-	return true;
-}
+/* char[] fixTime(char inp[], int len, int delta) { */
+/* 	char out[len]; */
+/* 	for(int i = 0; i < len; i++){ */
+/* 		switch (inp[i]) { */
+/* 			case '1': */
+/* 			case '4': */
+/* 			case '7': */
+/* 				out[i] = inp[i] + delta; */
+/* 		} */
+/* 	} */
+/* 	return out; */
+/* } */
 void run(){
-	char temp[3];
+	const int len = 3;
+	char temp[len];
 	cout << "Enter temperature 000-999: ";
 	cin >> temp;
-	doesntWork(temp);
+	if (doesntWork(temp, 3)) {
+		// sets all elements to number
+		// + 1 to null terminate string
+		char nextHighest[len + 1] = { 0 };	
+		char nextLowest[len + 1] = { 0 };	
+		bool flag = false;
+		for(int i = 0; i < len; i++){
+			if (flag) {
+				nextHighest[i] = '0';
+				nextLowest[i] = '9';
+				continue;
+			}
+			switch (temp[i]) {
+				case '1':
+				case '4':
+				case '7':
+					flag = true;
+					nextHighest[i] = temp[i] + 1;
+					nextLowest[i] = temp[i] - 1;
+				break;
+				default:
+					nextHighest[i] = nextLowest[i] = temp[i];
+				break;
+			}
+		}
+		cout << "Next highest: " << nextHighest << endl;
+		cout << "Next lowest: " << nextLowest << endl;
+	} else {
+		cout << "Temperature is valid!\nTemperature: " << temp;
+	}
 }
 
 int main(){
