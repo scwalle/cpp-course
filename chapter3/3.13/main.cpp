ae3e032b1067f70dbfe9e4725194693fdbe19e7f
scwalle <scwaller@bluevalleyk12.net>
2023-12-12 13:48:54 -0600

    3.13 progress

--- chapter3/3.13/main.cpp
+++ chapter3/3.13/main.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
+#include <format>
 using namespace std;
 
-int main()
-{
-    int num1, num2;
-    cout << "Enter 2 integers:\n";
-    cout << "1st integer:\n";
-    cin >> num1;
-    cout << "2nd integer:\n";
-    cin >> num2;
+bool allDifferent(string s){
+	int len = s.length();
+	if (len == 1 || len == 0) {
+		return true;
+	}
 
-    cout << "Sum:";
-    cout << num1 + num2;
-    cout << "\n";
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 1; j < len; j++) {
+			cout << "testing " << s[i] << " and " << s[j] << endl;
+			if (s[i] == s[j]) {
+				return true;
+			} else {
+				cout << s[i] << " !== " << s[j] << endl;
+			}
+		}
+	}
+	return false;
+}
 
-    cout << "Product: ";
-    cout << num1 * num2;
-    cout << "\n";
+string padTo(string s, int len){
+	string out;
+	return out.insert(0, '0', len - s.length());	
+}
 
-    return 0;
+int main(){
+	int i = 0;
+	/* string num = padTo(to_string(i), 4); */ 
+	string num = std::format("{:04}", i);
+	/* snprintf(num, 5, "%04d", i); */
+	cout << num << endl;
+	return 0;
+	while (
+			allDifferent(num)// &&
+	) {
+		cout << i++ << endl;
+		string num = padTo(to_string(i), 4); 
+	}
+	cout << "Answer: " << i;
 }
