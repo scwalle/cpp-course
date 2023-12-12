22b641b342ef62569c308a40c14b9b7fcece6ebd
Samuel C. Waller <scwaller@bluevalleyk12.net>
2023-12-12 14:24:48 -0600

    project 3.13\; chapter 3 done

--- chapter3/3.13/main.cpp
+++ chapter3/3.13/main.cpp
@@ -2,8 +2,9 @@
 #include <format>
 using namespace std;
 
+int const len = 4;
+
 bool allDifferent(string s){
-	int len = s.length();
 	if (len == 1 || len == 0) {
 		return true;
 	}
@@ -12,32 +13,42 @@ bool allDifferent(string s){
 		for (int j = i + 1; j < len; j++) {
 			cout << "testing " << s[i] << " and " << s[j] << endl;
 			if (s[i] == s[j]) {
-				return true;
+				cout << s[i] << " == " << s[j] << endl;
+				cout << "Fail!\n";
+				return false;
 			} else {
-				cout << s[i] << " !== " << s[j] << endl;
+				cout << s[i] << " != " << s[j] << endl;
 			}
 		}
 	}
-	return false;
+	return true;
 }
 
-string padTo(string s, int len){
-	string out;
-	return out.insert(0, '0', len - s.length());	
+bool is3Times(char s[]) {
+	return ( s[0] - '0' == 3 * (s[2] - '0') );
+}
+
+int sumDigits(char s[]) {
+	int sum = 0;
+	for (int i = 0; s[i]; i++) {
+		sum += s[i] - '0';	
+	}
+	return sum;
 }
 
 int main(){
 	int i = 0;
-	/* string num = padTo(to_string(i), 4); */ 
-	string num = std::format("{:04}", i);
-	/* snprintf(num, 5, "%04d", i); */
+	char num[len + 1] = { 0 };
+	snprintf(num, 5, "%04d", i);
 	cout << num << endl;
-	return 0;
-	while (
-			allDifferent(num)// &&
-	) {
-		cout << i++ << endl;
-		string num = padTo(to_string(i), 4); 
+	for (;!(
+				allDifferent(num) 
+				&& is3Times(num) 
+				&& (i % 2 == 0) 
+				&& (sumDigits(num) == 27)
+		   ); i++) {
+		snprintf(num, 5, "%04d", i);
+		cout << num << endl;
 	}
-	cout << "Answer: " << i;
+	cout << "Answer: " << num;
 }
