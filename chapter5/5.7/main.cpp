053b8450e610762f1e505d2c132cceb669d7fee6
scwalle <scwaller@bluevalleyk12.net>
2024-01-29 18:14:06 -0500

    5.7 progress

--- chapter5/5.7/main.cpp
+++ chapter5/5.7/main.cpp
@@ -9,6 +9,8 @@ int getYearValue(int year);
 
 int getMonthValue(int month, int year);
 
+int getDayOfWeek(int day, int month, int year);
+
 void doCode(){
 	int hours, mins;
 	cout << "Enter time:\n";
@@ -62,11 +64,92 @@ int getYearValue(int year) {
 	return last2 / 4 + last2;	
 };
 
+/*
+Month		Return Value
+-------------------------
+January 	0 (6 if year is a leap year)
+February 	3 (2 if year is a leap year)
+March 		3
+April 		6
+May 		1
+June 		4
+July 		6
+August 		2
+September 	5
+October 	0
+November 	3
+December 	5
+*/
+enum Months {
+	January,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December
+};
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 int getMonthValue(int month, int year) {
-	switch(){
-		case "January":
+	switch(month){
+		case January:
+			return isLeapYear(year) ? 6 : 0;
+		break;
+		case February:
+			return isLeapYear(year) ? 2 : 3;
+		break;
+		case March:
+			return 3;
+		break;
+		case April:
+			return 6;
+		break;
+		case May:
+			return 1;
+		break;
+		case June:
+			return 4;
+		break;
+		case July:
+			return 6;
+		break;
+		case August:
+			return 2;
+		break;
+		case September:
+			return 5;
+		break;
+		case October:
 			return 0;
+		break;
+		case November:
+			return 3;
+		break;
+		case December:
+			return 5;
 		break;
 	}
 };
+
+int getDayOfWeek(int day, int month, int year){
+	return (day + getMonthValue(month, year) + getYearValue(year) + getCenturyValue(year)) % 7;
+}
 
