8665edba4749f0d401485c5765917beb33a1d97d
scwalle <scwaller@bluevalleyk12.net>
2024-03-28 17:39:16 -0500

    complete 6.4

--- chapter6/6.4/main.cpp
+++ chapter6/6.4/main.cpp
@@ -1,39 +1,56 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-void doCode(){
-	int hours, mins;
-	cout << "Enter time:\n";
-	cin >> hours;
-	cin.ignore(); // ignore colon. this is a bad way to do this
-	cin >> mins;
-	char day1, day2;
-	cout << "Enter day of week:";
-	cin >> day1 >> day2;
-	cout << day1 << endl;
-	cout << day2 << endl;
-
-
-	cout << "Sum:";
-	cout << hours + mins;
-	cout << "\n";
-
-	cout << "Product: ";
-	cout << hours * mins;
-	cout << "\n";
-}
-	
-int main(){
-	cout.setf(ios::fixed);
-	cout.setf(ios::showpoint);
-	cout.precision(2);
-	while(true){
-		doCode();
-		
-		cout << "\nTry again? (Ctrl-C to exit, anything else to go again): ";
-		string loop = "";
-		cin >> loop;
-		if (loop == "") return 0;
-		cout << endl << endl;
+bool isWordSep (char c) {
+	switch (c) {
+		case ' ':
+		case '.':
+		case ',':
+		case '\n':
+			return true;
+			break;
+		default:
+			return false;
 	}
+}
+
+double computeAverageWord(istream &input){
+	char c;
+	int charCount = 0;
+	int wordCount = 0;
+	int currWord = 0;
+	input.get(c);
+	while (!input.eof()) {
+		if (!isWordSep(c)) {
+			currWord++;
+		}
+		if (isWordSep(c) && currWord != 0) {
+			wordCount++;
+			charCount += currWord;
+			currWord = 0;
+		}
+		input.get(c);
+	}
+	return ((double) charCount) / ((double) wordCount);
+}
+
+	
+int main(int argc, char *argv[]){
+	double avglen;
+	if (argc > 1) {
+		ifstream input;
+
+		input.open(argv[1]);
+		if (input.fail()) {
+			cerr << "failed to open file '" << argv[1] << "'.\n";
+			exit(1);
+		}
+		avglen = computeAverageWord(input);
+		input.close();
+	} else {
+		avglen = computeAverageWord(cin);
+	}
+	cout << "Average word length: " << avglen << endl;
 }
