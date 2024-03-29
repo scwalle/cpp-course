e1bb452971f46e71ec93076bd972246eb71f1fd7
scwalle <scwaller@bluevalleyk12.net>
2024-03-29 14:34:50 -0500

    complete 6.13

--- chapter6/6.13/main.cpp
+++ chapter6/6.13/main.cpp
@@ -1,39 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <cctype>
 using namespace std;
 
-void doCode(){
-	int hours, mins;
-	cout << "Enter time:\n";
-	cin >> hours;
-	cin.ignore(); // ignore colon. this is a bad way to do this
-	cin >> mins;
-	char day1, day2;
-	cout << "Enter day of week:";
-	cin >> day1 >> day2;
-	cout << day1 << endl;
-	cout << day2 << endl;
+int countWords(istream &in) {
+	char c;
+	in.get(c);
+	bool inword = false;
 
-
-	cout << "Sum:";
-	cout << hours + mins;
-	cout << "\n";
-
-	cout << "Product: ";
-	cout << hours * mins;
-	cout << "\n";
-}
-	
-int main(){
-	cout.setf(ios::fixed);
-	cout.setf(ios::showpoint);
-	cout.precision(2);
-	while(true){
-		doCode();
-		
-		cout << "\nTry again? (Ctrl-C to exit, anything else to go again): ";
-		string loop = "";
-		cin >> loop;
-		if (loop == "") return 0;
-		cout << endl << endl;
+	int wc = 0;
+	while (!in.eof()) {
+		if (isspace(c)) {
+			inword = false;
+		} else {
+			if (!inword) {
+				wc++;
+			}
+			inword = true;
+		}
+		in.get(c);
 	}
+	return wc;
+}
+
+int main(int argc, char *argv[]){
+	ifstream input;
+	
+	if (argc < 2) {
+		cout << "Not enough arguments\n";
+		cout << "Usage: " << argv[0] << " filenames\n";
+		exit(1);
+	}
+	const int filesnum = argc - 1;
+
+	for (int i = 1; i < argc; i++) {
+		char* fname = argv[i];
+		input.open(fname);
+		if (input.fail()) {
+			cerr << "Can't open file: " << fname << endl;
+			exit(1);
+		}
+		int wc = countWords(input);
+		input.close();
+		cout << fname << ": " << wc << endl;
+	}
+
+	return 0;
 }
