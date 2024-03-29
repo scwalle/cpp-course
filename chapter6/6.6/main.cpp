4229d53485caac89858615a30fef8cc96fbe104d
scwalle <scwaller@bluevalleyk12.net>
2024-03-29 10:18:42 -0500

    complete 6.6

--- chapter6/6.6/main.cpp
+++ chapter6/6.6/main.cpp
@@ -1,39 +1,72 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void doCode(){
-	int hours, mins;
-	cout << "Enter time:\n";
-	cin >> hours;
-	cin.ignore(); // ignore colon. this is a bad way to do this
-	cin >> mins;
-	char day1, day2;
-	cout << "Enter day of week:";
-	cin >> day1 >> day2;
-	cout << day1 << endl;
-	cout << day2 << endl;
+const int NUMBER_OF_CHAPTERS = 18;
 
+void printWisdom(ifstream &wisdom);
+void new_line(istream& in_stream = cin);
 
-	cout << "Sum:";
-	cout << hours + mins;
-	cout << "\n";
-
-	cout << "Product: ";
-	cout << hours * mins;
-	cout << "\n";
-}
-	
-int main(){
-	cout.setf(ios::fixed);
-	cout.setf(ios::showpoint);
-	cout.precision(2);
-	while(true){
-		doCode();
-		
-		cout << "\nTry again? (Ctrl-C to exit, anything else to go again): ";
-		string loop = "";
-		cin >> loop;
-		if (loop == "") return 0;
-		cout << endl << endl;
+const string WISDOM_FILE = "wisdom.txt";
+int main() {
+	ifstream input;
+	input.open(WISDOM_FILE);
+	if (input.fail()) {
+		cerr << "can't read input file\n";
+		exit(1);
 	}
+	
+	cout << "What is your question? ";	
+	new_line(cin);
+	while (!cin.eof()) {
+		printWisdom(input);
+		cout << endl;
+		cout << "What is your question? ";	
+		new_line();
+	}
+
+
+	input.close();
+}
+
+void printWisdom(ifstream &wisdom){
+	static int chapternum = NUMBER_OF_CHAPTERS;
+	char c;
+	wisdom.get(c);
+	while (c != '\n' && !wisdom.eof()) {
+		if (c == '#') {
+			// matched "#"
+			wisdom.get(c);
+			if (c == 'N') {
+				// matched "#N"
+				cout << chapternum;
+			} else {
+				cout << '#' << c;
+			}
+		} else {
+			cout.put(c);
+		}
+		wisdom.get(c);
+	}
+
+	if (wisdom.eof()) {
+		wisdom.close();
+		wisdom.open(WISDOM_FILE);
+
+		chapternum--;
+		if (chapternum == 0) {
+			chapternum = NUMBER_OF_CHAPTERS;
+		}
+
+		printWisdom(wisdom);
+	}
+}
+
+void new_line(istream& in_stream)
+{
+	char symbol;
+	do
+	{
+		in_stream.get(symbol);
+	} while (symbol != '\n' && !in_stream.eof());
 }
