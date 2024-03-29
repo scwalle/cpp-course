eb479e58cb11af0669ab4e2c077d139349e2ee5a
scwalle <scwaller@bluevalleyk12.net>
2024-03-29 14:08:28 -0500

    complete 6.11

--- chapter6/6.11/main.cpp
+++ chapter6/6.11/main.cpp
@@ -1,39 +1,86 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void doCode(){
-	int hours, mins;
-	cout << "Enter time:\n";
-	cin >> hours;
-	cin.ignore(); // ignore colon. this is a bad way to do this
-	cin >> mins;
-	char day1, day2;
-	cout << "Enter day of week:";
-	cin >> day1 >> day2;
-	cout << day1 << endl;
-	cout << day2 << endl;
+const int svgheight = 500;
+const int svgwidth = 500;
 
-
-	cout << "Sum:";
-	cout << hours + mins;
-	cout << "\n";
-
-	cout << "Product: ";
-	cout << hours * mins;
-	cout << "\n";
+/* 
+s;"\([^"]\+\)";\\"" << \1 << "\\";g
+*/
+void fillRect(ofstream &out, int x, int y, int width, int height) {
+	out << "<rect ";
+	out << "x=\"" << x << "\" ";
+	out << "y=\"" << y << "\" ";
+	out << "width=\"" << width << "\" ";
+	out << "height=\"" << height << "\" ";
+	/* out << "style=\"stroke:purple;stroke-width:2\""; */
+	/* out << "style=\"stroke:purple;stroke-width:2\""; */
+	out << "/>\n";
 }
+
+void line(ofstream &out, int x1, int y1, int x2, int y2) {
+	out << "<line ";
+	out << "x1=\"" << x1 << "\" ";
+	out << "y1=\"" << y1 << "\" ";
+	out << "x2=\"" << x2 << "\" ";
+	out << "y2=\"" << y2 << "\" ";
+	out << "style=\"stroke:black;stroke-width:2\"";
+	out << "/>\n";
+};
 	
-int main(){
-	cout.setf(ios::fixed);
-	cout.setf(ios::showpoint);
-	cout.precision(2);
-	while(true){
-		doCode();
-		
-		cout << "\nTry again? (Ctrl-C to exit, anything else to go again): ";
-		string loop = "";
-		cin >> loop;
-		if (loop == "") return 0;
-		cout << endl << endl;
+int main(int argc, char *argv[]){
+	if (argc <= 1) {
+		cout << "Not enough args\n";
+		exit(1);
 	}
+
+	ofstream output;
+	output.open("chart.svg");
+	if (output.fail()) {
+		cerr << "can't open output file\n";
+		exit(1);
+	}
+	output << 
+		"<?xml version=\"1.0\" standalone=\"no\"?>\n"
+		"<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \n"
+		"\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n"
+		"<svg width=\"" << 500 << "\" height=\"" << 500 << "\"\n"
+		"xmlns=\"http://www.w3.org/2000/svg\">\n";
+
+
+	line(output, 2, 0, 2, svgheight);
+	line(output, 2, svgheight, svgwidth, svgheight);
+
+	const int numcount = argc-1;
+	int nums[numcount];
+	
+	int max = 0;
+	for (int i=0; i < numcount; i++) {
+		nums[i] = stoi(argv[i+1]);
+
+		if (nums[i] > max) {
+			max = nums[i];
+		}
+	}
+
+	{
+		const int spacing = svgwidth / (numcount * 2 + 1);
+		const int maxheight = 400;
+		const int width = spacing;
+		/* const int width = 30; */
+
+		int x = spacing;
+		for (int num : nums) {
+			int height = (num * maxheight) / max;
+			fillRect(output, x, svgheight - height, width, height);
+			x += width + spacing;
+		}
+	}
+
+	output << "\n</svg>\n";
+
+
+	output.close();
 }
+
