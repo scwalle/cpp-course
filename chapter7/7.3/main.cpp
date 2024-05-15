070c782decb36765e9b445d26ef56ec4fd412b03
scwalle <scwaller@bluevalleyk12.net>
2024-05-15 15:35:17 -0500

    complete 7.3

--- chapter7/7.3/main.cpp
+++ chapter7/7.3/main.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
 using namespace std;
 
+
+void shift_array(char array[], int pos, int length) {
+	for (int i = pos+1; i < length; i++) {
+		array[i-1] = array[i];
+	}
+	array[length-1] = 0;
+}
+
+void delete_repeats(char array[], int &size) {
+	const int letter_num = 26;
+	int letters[letter_num];
+	for (int i = 0; i < letter_num; i++) {
+		letters[i] = 0;
+	}
+
+	for (int i = 0; i < size; i++) {
+		/* cout << (i) << endl; */
+		/* cout << (array[i]) << endl; */
+		/* cout << "(letters[ array[i] ]): " << (letters[ array[i] - 'a' ]) << endl; */
+		if (letters[array[i] - 'a']) {
+			/* cout << "seen!\n"; */
+			array[i] = '0';
+			/* cout << array << endl; */
+			shift_array(array, i, size);
+			size--;
+			i--; // current letter changed, so go back to not miss it
+		} else {
+			letters[array[i] - 'a'] = 1;
+		}
+	}
+}
+
 void doCode(){
-	int hours, mins;
-	cout << "Enter time:\n";
-	cin >> hours;
-	cin.ignore(); // ignore colon. this is a bad way to do this
-	cin >> mins;
-	char day1, day2;
-	cout << "Enter day of week:";
-	cin >> day1 >> day2;
-	cout << day1 << endl;
-	cout << day2 << endl;
+	int size;
+	cout << "Enter array size:";
+	cin >> size;
+	char array[size+1];
+	array[size] = 0;
+	for (int i = 0; i < size; i++) {
+		cout << "Element " << i << ":";
+		cin >> array[i];
+	}
+	cout << endl;
+	cout << "array: " << array << "\n";
+	delete_repeats(array, size);
+	/* shift_array(array, 0, size); */
+	cout << "array: " << array << "\n";
 
-
-	cout << "Sum:";
-	cout << hours + mins;
-	cout << "\n";
-
-	cout << "Product: ";
-	cout << hours * mins;
-	cout << "\n";
 }
 	
-int main(){
+int main(int argc, char *argv[]){
 	cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(2);
