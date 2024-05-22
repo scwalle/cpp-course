d2c814e7aaf7f6b4d01e1d721a2f14b05c184dfb
scwalle <scwaller@bluevalleyk12.net>
2024-05-22 15:13:00 -0500

    complete 8.16

--- chapter8/8.16/main.cpp
+++ chapter8/8.16/main.cpp
@@ -1,26 +1,160 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+const int SENSORCOUNT = 3;
+struct time24h {
+	int hours;
+	int mins;
+	int secs;
+};
+
+const double DISTANCE = 13.1;
+const double SENSOR_SPACING = DISTANCE / (SENSORCOUNT - 1);
+
+struct Racer {
+	time24h times[3];
+	int id;
+};
+
+void outputTime(struct time24h &time){
+	printf("%02d:%02d:%02d", time.hours, time.mins, time.secs);
+}
+
+
+int findRacer(vector<Racer> list, int id) {
+	for (int i = 0; i < list.size(); i++) {
+		if (list[i].id == id) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+double toMinutes(time24h time) {
+	double mins = 0;
+	mins += time.hours * 60;
+	mins += time.mins;
+	mins += (time.secs / 60.0);
+	return mins;
+}
+
+double findSplit(time24h start, time24h end) {
+	return toMinutes(end) - toMinutes(start);
+}
+
+// returns time it took to finish race in mins
+double finishTime(Racer r) {
+	time24h start = r.times[0];
+	time24h end = r.times[SENSORCOUNT - 1];
+	return findSplit(end, start);
+}
+
+bool fasterThan(Racer a, Racer b) {
+	return finishTime(a) < finishTime(b);
+}
+
+void printRacer(Racer racer) {
+	cout << "Racer #" << racer.id << endl;
+	for (int i = 0; i < SENSORCOUNT; i++) {
+		cout << "Sensor " << i << " time: ";
+		outputTime(racer.times[i]);
+		cout << endl;
+	}
+}
+
+void printRacerFull(Racer racer) {
+	cout << "Racer #" << racer.id << endl;
+	for (int i = 0; i < SENSORCOUNT; i++) {
+		cout << "Sensor " << i << " time: ";
+		outputTime(racer.times[i]);
+		if (i != 0) {
+			cout << "  Split: " 
+				<< (findSplit(racer.times[i-1], racer.times[i]) / SENSOR_SPACING)
+				<< " mins/mile";
+		}
+		cout << endl;
+	}
+}
+
 void doCode(){
-	int hours, mins;
-	cout << "Enter time:\n";
-	cin >> hours;
-	cin.ignore(); // ignore colon. this is a bad way to do this
-	cin >> mins;
-	char day1, day2;
-	cout << "Enter day of week:";
-	cin >> day1 >> day2;
-	cout << day1 << endl;
-	cout << day2 << endl;
+	vector<Racer> racers;
+	time24h gunTime;
+
+	ifstream input;
+	input.open("sensordata.txt");
+
+	input >> gunTime.hours >> gunTime.mins >> gunTime.secs;
+
+	while (!input.fail()) {
+		int sensor, racerId;	
+		Racer racer;
+		char comma;
 
 
-	cout << "Sum:";
-	cout << hours + mins;
-	cout << "\n";
 
-	cout << "Product: ";
-	cout << hours * mins;
-	cout << "\n";
+		input >> sensor >> comma 
+			  >> racerId >> comma;
+
+		int racerIndex = findRacer(racers, racerId);
+		if (racerIndex == -1) {
+			racers.push_back(racer);
+			racerIndex = racers.size()-1;
+			racer.id = racerId;
+		} else {
+			racer = racers[racerIndex];
+		}
+
+		time24h *time = &racer.times[sensor];
+
+		input >> time->hours >> time->mins >> time->secs;
+		/* racer.times[sensor] = time; */
+
+
+		racers[racerIndex] = racer;
+		/* printRacer(racer); */
+	}
+	input.close();
+	/* cout << "----------------------------\n"; */
+
+	sort(racers.begin(), racers.end(), fasterThan);
+
+	/* for (Racer r : racers) { */
+	/* 	printRacerFull(r); */
+	/* } */
+
+	int racerNum;
+	cout << "Enter racer number: ";
+	cin >> racerNum;
+	int index = findRacer(racers, racerNum);
+	if (index == -1) {
+		cout << "Invalid racer number.\n";
+		return;
+	}
+	Racer r = racers[index];
+
+	cout << endl;
+	printRacerFull(r);
+
+	int place = index + 1;
+	cout << "\nRacer placed " << place
+		<< ((place == 1) ? "st" 
+				: ( (place == 2) ? "nd"
+				   : ((place == 3) ? "rd" 
+					   : "th"
+					   )
+				  )
+			)
+		<< endl;
+
+
+	// format:
+	// 1st line: gun time in HH MM SS format
+	// rest of lines:
+	// sensorId,racerId,timeStamp(in HH MM SS)
+
 }
 	
 int main(){
